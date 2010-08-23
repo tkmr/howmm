3d1cac265809bcd22aff789939641a80e447fb62
Tatsuya Takamura <tatsuya@tkmr.org>
2010-08-24 03:34:28 +0900

    improve some UI

--- couchapp/howmm/evently/doc/edit/after.js
+++ couchapp/howmm/evently/doc/edit/after.js
@@ -14,35 +14,43 @@ function(){
     self.trigger('show', [new_doc]);
   }
 
-  var submit_func = function(stop_chain){
-    return function(){
-      if(old_body !== textarea[0].value){
-        old_body = textarea[0].value;
+  var save_doc = function(switch_to_show){
+    if(old_body !== textarea[0].value){
+      old_body = textarea[0].value;
 
-        var doc        = $.extend(old_data, form.serializeObject());
-        doc.title      = doc.body.split("\n")[0];
-        doc.updated_at = new Date();
-        doc.created_at = doc.created_at || doc.updated_at;
+      var doc        = $.extend(old_data, form.serializeObject());
+      doc.title      = doc.body.split("\n")[0];
+      doc.updated_at = new Date();
+      doc.created_at = doc.created_at || doc.updated_at;
 
-        var new_doc = doc._id ? false : true;
-        howmm.db.saveDoc(doc, function(save_resp) {
-          howmm.util.message("saved doc!!", self);
+      var new_doc = doc._id ? false : true;
+      howmm.db.saveDoc(doc, function(save_resp) {
+        howmm.util.message("saved doc!!", self);
+        if(switch_to_show){
           setTimeout(function(){ go_to_show(save_resp) }, 600);
-        });
-      }
+        }
+      });
+    }
+  };
+
+  var submit_action = function(stop_chain){
+    return function(){
+      save_doc(true);
       return !stop_chain;
-    };
+    }
   }
+
+  form.bind('submit', submit_action(true));
+  textarea.bind('blur', submit_action(false));
 
   self.find("a.cancel_edit").click(function(){
     go_to_show(old_data);
   });
 
-  form.bind('submit', submit_func(true));
-  textarea.bind('blur', submit_func(false));
-
   if(zaisu.util.is_ios()){
-    setTimeout(textarea.focus, 10);
+    setTimeout(function(){
+      //textarea[0].focus();
+    }, 50);
   }else{
     textarea.focus();
   }
